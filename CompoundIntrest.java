ccfe99eb6944ee010e1b335da2a1850014e9d5c8
Mohammed Mukarram <53473599+ryuoga12@users.noreply.github.com>
2020-04-22 12:54:16 +0530

    Update CompoundIntrest.java

--- CompoundIntrest.java
+++ CompoundIntrest.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 public class CI {
 
-    public void calculate(int p, int t, double r, int n) {
+    public void calculate(int p, int t, double r, int n) 
+    {
         double a = p * Math.pow(1 + (r / n), n * t);
         double cinterest = a - p;
         System.out.println("Compound Interest after " + t + " years: "+cinterest);
@@ -13,11 +14,11 @@ public class CI {
       double r;
     	CI obj = new CI();
       Scanner s=new Scanner(System.in);
-      System.out.print("Enter the principal amount : ");
+      System.out.print("Enter  principal amount : ");
       p=s.nextInt();
-      System.out.print("Enter the time the money is invested or borrowed for : ");
+      System.out.print("Enter  time the money is invested or borrowed for : ");
       t=s.nextInt();
-      System.out.print("Enter the the annual interest rate : ");
+      System.out.print("Enter  the annual interest rate : ");
       r=s.nextDouble();
       System.out.print("Enter  the number of times that interest is compounded per unit t : ");
       n=s.nextInt();
